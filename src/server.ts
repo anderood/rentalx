dd9425d72350df0d748f31f74d47f6bf1fc716fa
eunerd <anderood@hotmail.com>
2023-04-07 01:45:20 -0300

    adjusts

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,9 @@ import express from 'express';
 
 const app = express();
 
+app.get("/route", (request, response) =>{
+    console.log('teste');
+})
+
 app.listen(3333, () => console.log('Server is Running'));
 
